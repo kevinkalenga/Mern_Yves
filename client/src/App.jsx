3a8adae308211508c3aade2c22022c2b9fcbc625
kevin kalenga <kevinkalenga10@gmail.com>
2024-12-16 14:54:36 +0100

    Create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 
 export default function App() {
   return (
-    <h1 className="bg-red-700 text-center text-white py-2">
-      Mon super site
-    </h1>
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+            
+          </Routes>
+    </BrowserRouter>
   )
 }
